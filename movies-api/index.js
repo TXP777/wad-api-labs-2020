9f572f4f4a3bfdfd6d92166c31a9c6fb166e5d48
Xianping Tao <20091578@mail.wit.ie>
2021-01-03 17:52:49 +0000

    Week 9 Exercises

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import genresRouter from './api/genres';
 import bodyParser from 'body-parser';
 import './db';
 import {loadUsers} from './seedData';
@@ -31,6 +32,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use('/api/movies', moviesRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/genres',genresRouter)
 app.use(errHandler);
 
 
